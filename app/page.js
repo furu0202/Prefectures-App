719c9e3fbd01ca44f445c6db7f9533e21825b519
furu0202 <furuka.0202@gmail.com>
2023-07-06 23:44:16 +0900

    タイトル変更

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>都道府県クイズ</h1>
+    </main>
+  );
 }
