4f0884f2db7ca2b28498a66ec55283dada35610c
furu0202 <furuka.0202@gmail.com>
2023-07-09 00:23:36 +0900

    背景画像の編集

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 import Head from 'next/head';
+import styles from './page.module.css';
+import Prefectures from '../app/image/Prefectures.jpg';
 
 export default function Home() {
   return (
-    <main>
-      <h1>都道府県クイズ</h1>
+    <main className={styles.main}>
+      <div className={styles.title}>
+        <h1>都道府県クイズ</h1>
+      </div>
+      <div className={styles.button}>
+        <button>スタート</button>
+      </div>
     </main>
   );
 }
