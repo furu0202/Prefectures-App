c48c2e9db91b16ac635644c18a7ce58b665ea4e4
furu0202 <furuka.0202@gmail.com>
2023-07-12 23:52:08 +0900

    難易度を決める３種類のボタンを追加

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './page.module.css';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.buttonBeginner}>
+        <button>初級編</button>
+      </div>
+      <div className={styles.buttonIntermediate}>
+        <button>中級編</button>
+      </div>
+      <div className={styles.buttonAdvanced}>
+        <button>上級編</button>
+      </div>
+    </main>
+  );
 };
 
 export default page;
