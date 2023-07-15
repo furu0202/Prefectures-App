ee1c05559e9d75641c7cb536ccd7c847c696c6f3
furu0202 <furuka.0202@gmail.com>
2023-07-15 19:00:45 +0900

    見出しの追加

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -3,17 +3,20 @@ import styles from './page.module.css';
 
 const page = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.buttonBeginner}>
-        <button>初級編</button>
-      </div>
-      <div className={styles.buttonIntermediate}>
-        <button>中級編</button>
-      </div>
-      <div className={styles.buttonAdvanced}>
-        <button>上級編</button>
-      </div>
-    </main>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>難易度選択</h1>
+        <div className={styles.buttonBeginner}>
+          <button>初級編</button>
+        </div>
+        <div className={styles.buttonIntermediate}>
+          <button>中級編</button>
+        </div>
+        <div className={styles.buttonAdvanced}>
+          <button>上級編</button>
+        </div>
+      </main>
+    </div>
   );
 };
 
