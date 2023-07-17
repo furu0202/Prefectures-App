ce169c22ddc03cb94b6ad343751c9e746a6bb1c0
furu0202 <furuka.0202@gmail.com>
2023-07-17 23:21:03 +0900

    ボタンの配置を縦並びから横並びへ変更

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -7,14 +7,16 @@ const page = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>難易度選択</h1>
-        <div className={styles.buttonBeginner}>
-          <Link href={'beginner'}>初級編</Link>
-        </div>
-        <div className={styles.buttonIntermediate}>
-          <Link href={'intermediate'}>中級編</Link>
-        </div>
-        <div className={styles.buttonAdvanced}>
-          <Link href={'advanced'}>上級編</Link>
+        <div className={styles.buttonArea}>
+          <div className={styles.buttonBeginner}>
+            <Link href={'beginner'}>初級編</Link>
+          </div>
+          <div className={styles.buttonIntermediate}>
+            <Link href={'intermediate'}>中級編</Link>
+          </div>
+          <div className={styles.buttonAdvanced}>
+            <Link href={'advanced'}>上級編</Link>
+          </div>
         </div>
       </main>
     </div>
