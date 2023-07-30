4feb4c70980b0ca7eb4da9e51097e2a6a008648b
furu0202 <furuka.0202@gmail.com>
2023-07-30 23:27:27 +0900

    不必要となった項目を削除

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -2,22 +2,6 @@ import React from 'react';
 import styles from './page.module.css';
 import Link from 'next/link';
 
-const ITEMS = [
-  {
-    level: '初級',
-    explanation: ``,
-  },
-
-  {
-    level: '中級',
-    explanation: ``,
-  },
-
-  {
-    level: '上級',
-    explanation: ``,
-  },
-];
 const page = () => {
   return (
     <div className={styles.container}>
@@ -25,16 +9,19 @@ const page = () => {
       <main className={styles.main}>
         <h1>難易度選択</h1>
         <div className={styles.buttonArea}>
-          <div>
+          <div className={styles.beginner}>
+            <p className={styles.begginerTitle}>説明文</p>
             <Link className={styles.buttonBeginner} href={'beginner'}></Link>
           </div>
-          <div>
+          <div className={styles.intermediate}>
+            <p>説明文</p>
             <Link
               className={styles.buttonIntermediate}
               href={'intermediate'}
             ></Link>
           </div>
-          <div>
+          <div className={styles.advanced}>
+            <p>説明文</p>
             <Link className={styles.buttonAdvanced} href={'advanced'}></Link>
           </div>
         </div>
