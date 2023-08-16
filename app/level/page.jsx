51ecedbc3cf57b1ab44936970b389f1ef1ee2705
furu0202 <furuka.0202@gmail.com>
2023-08-16 23:27:09 +0900

    pタグからh2タグへ変更

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -30,19 +30,19 @@ const page = () => {
         <h1>難易度選択</h1>
         <div className={styles.buttonArea}>
           <div className={styles.beginner}>
-            <p className={styles.begginerTitle}>初級編</p>
+            <h2 className={styles.begginerTitle}>初級編</h2>
             <Link className={styles.buttonBeginner} href={'beginner'}>
               スタート
             </Link>
           </div>
           <div className={styles.intermediate}>
-            <p className={styles.intermediateTitle}>中級編</p>
+            <h2 className={styles.intermediateTitle}>中級編</h2>
             <Link className={styles.buttonIntermediate} href={'intermediate'}>
               スタート
             </Link>
           </div>
           <div className={styles.advanced}>
-            <p className={styles.advancedTitle}>上級編</p>
+            <h2 className={styles.advancedTitle}>上級編</h2>
             <Link className={styles.buttonAdvanced} href={'advanced'}>
               スタート
             </Link>
