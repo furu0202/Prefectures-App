276a417157869171f324c4e837eb4705b584fda9
furu0202 <furuka.0202@gmail.com>
2023-10-03 23:50:37 +0900

    ITEMS コンポーネント作成

---
+++ app/components/Items.js
@@ -0,0 +1,40 @@
+const ITEMS = [
+  {
+    id: '1',
+    name: 'START',
+    title: '日本地図クイズ',
+    href: '/jpnmap',
+  },
+
+  {
+    id: '2',
+    name: 'START',
+    title: '地名クイズ',
+    href: '/placename',
+  },
+
+  {
+    id: '3',
+    name: 'START',
+    title: '雑学クイズ',
+    href: '/trivia',
+  },
+  {
+    id: '4',
+    name: 'START',
+    title: '駅名クイズ',
+    href: '/stationname',
+  },
+  {
+    id: '5',
+    name: 'START',
+    title: '方言クイズ',
+    href: '/dialect',
+  },
+  {
+    id: '6',
+    name: 'START',
+    title: '県庁所在地クイズ',
+    href: '/prefecturalcapital',
+  },
+];
