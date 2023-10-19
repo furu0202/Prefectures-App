0eb151589f42d5460cc600a94d12b10d6e919f4d
furu0202 <furuka.0202@gmail.com>
2023-10-19 23:44:37 +0900

    classNameの設定、cssのimport

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -2,18 +2,19 @@
 
 import React from 'react';
 import { usePathname } from 'next/navigation';
+import { styles } from './page.module.css';
 import { ITEMS } from '../components/Items';
 
 const Page = () => {
   const pathname = usePathname();
   const pathData = ITEMS.find((item) => item.href === pathname);
   return (
-    <div>
+    <div className={styles.main}>
       <h1>{pathData.title}</h1>
       <h1>第X問</h1>
       <h2>現在の正解数</h2>
       <h2>問題文{pathData.question}</h2>
-      <div>
+      <div className={styles.choicesArea}>
         <p>選択肢A</p>
         <p>選択肢B</p>
         <p>選択肢C</p>
