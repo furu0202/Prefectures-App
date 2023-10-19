6518db73ba6b9101c2e3f7a2817ef2139e3ce469
furu0202 <furuka.0202@gmail.com>
2023-10-20 03:08:06 +0900

    背景色とクイズタイトルを編集

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,24 +1,26 @@
 'use client';
 
 import React from 'react';
+import styles from './page.module.css';
 import { usePathname } from 'next/navigation';
-import { styles } from './page.module.css';
 import { ITEMS } from '../components/Items';
 
 const Page = () => {
   const pathname = usePathname();
   const pathData = ITEMS.find((item) => item.href === pathname);
   return (
-    <div className={styles.main}>
-      <h1>{pathData.title}</h1>
-      <h1>第X問</h1>
-      <h2>現在の正解数</h2>
-      <h2>問題文{pathData.question}</h2>
-      <div className={styles.choicesArea}>
-        <p>選択肢A</p>
-        <p>選択肢B</p>
-        <p>選択肢C</p>
-        <p>選択肢D</p>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1 className={styles.title}>{pathData.title}</h1>
+        <h1>第X問</h1>
+        <h2>現在の正解数</h2>
+        <h2>問題文{pathData.question}</h2>
+        <div>
+          <p>選択肢A</p>
+          <p>選択肢B</p>
+          <p>選択肢C</p>
+          <p>選択肢D</p>
+        </div>
       </div>
     </div>
   );
