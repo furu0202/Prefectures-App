bf2f40231b2790342dddfe75cd81033cc9089fac
furu0202 <furuka.0202@gmail.com>
2023-10-24 23:49:12 +0900

    サンプル問題を作成

---
+++ app/quiz/stationname.js
@@ -0,0 +1,47 @@
+const quizData = [
+  {
+    title: '「四条畷」この駅名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'よんじょうなわ', isCorrect: false },
+      { textResponse: 'しじょうなわて', isCorrect: true },
+      { textResponse: 'よつじょうわら', isCorrect: false },
+      { textResponse: 'よじょうわら', isCorrect: false },
+    ]),
+  },
+  {
+    title: '「喜連瓜破」この駅名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'きつらうつわ', isCorrect: false },
+      { textResponse: 'きれうりわり', isCorrect: true },
+      { textResponse: 'きれんうつは', isCorrect: false },
+      { textResponse: 'きりうつわり', isCorrect: false },
+    ]),
+  },
+  {
+    title: '「柘植」この駅名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'きうえ', isCorrect: false },
+      { textResponse: 'やし', isCorrect: false },
+      { textResponse: 'きしょく', isCorrect: false },
+      { textResponse: 'つげ', isCorrect: true },
+    ]),
+  },
+  {
+    title: '「勝原」この駅名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'まさはら', isCorrect: false },
+      { textResponse: 'かちはら', isCorrect: false },
+      { textResponse: 'いくさはら', isCorrect: false },
+      { textResponse: 'かどはら', isCorrect: true },
+    ]),
+  },
+  {
+    title: '「小牛田」この駅名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'こごた', isCorrect: true },
+      { textResponse: 'しぎゅう', isCorrect: false },
+      { textResponse: 'ぎょうた', isCorrect: false },
+      { textResponse: 'おうた', isCorrect: false },
+    ]),
+  },
+];
