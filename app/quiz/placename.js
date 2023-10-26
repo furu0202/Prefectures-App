24dfac2a9b07dfc24768165e6c309750efd7c0b1
furu0202 <furuka.0202@gmail.com>
2023-10-26 23:57:42 +0900

    地名クイズと方言クイズのサンプル問題を作成

---
+++ app/quiz/placename.js
@@ -0,0 +1,47 @@
+const quizData = [
+  {
+    title: '「熱海」この地名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'ねつみ', isCorrect: false },
+      { textResponse: 'あたみ', isCorrect: true },
+      { textResponse: 'あたか', isCorrect: false },
+      { textResponse: 'あつみ', isCorrect: false },
+    ]),
+  },
+  {
+    title: '「留萌」この地名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'りゅもん', isCorrect: false },
+      { textResponse: 'るもい', isCorrect: true },
+      { textResponse: 'るる', isCorrect: false },
+      { textResponse: 'とめ', isCorrect: false },
+    ]),
+  },
+  {
+    title: '「川内」この地名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'せお', isCorrect: true },
+      { textResponse: 'せんだい', isCorrect: false },
+      { textResponse: 'せんない', isCorrect: false },
+      { textResponse: 'せない', isCorrect: false },
+    ]),
+  },
+  {
+    title: '「赤穂」この地名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'せほう', isCorrect: false },
+      { textResponse: 'せきほう', isCorrect: false },
+      { textResponse: 'あがほ', isCorrect: false },
+      { textResponse: 'あこう', isCorrect: true },
+    ]),
+  },
+  {
+    title: '「我孫子」この駅名を答えよ',
+    options: arrayShuffle([
+      { textResponse: 'あまち', isCorrect: false },
+      { textResponse: 'わこうど', isCorrect: false },
+      { textResponse: 'あびこ', isCorrect: true },
+      { textResponse: 'あまご', isCorrect: false },
+    ]),
+  },
+];
