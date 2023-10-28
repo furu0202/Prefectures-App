991a6fb9f236020a72af5e7f5f4143fc04932501
furu0202 <furuka.0202@gmail.com>
2023-10-28 23:26:34 +0900

    雑学クイズのサンプル問題作成

---
+++ app/quiz/trivia.js
@@ -0,0 +1,50 @@
+const quizData = [
+  {
+    title:
+      '2012年5月22日にグランドオープンした、地上デジタル放送などの電波を送信する世界一高い自立電波塔である「東京スカイツリー」があるのは東京都何区か答えよ',
+    options: arrayShuffle([
+      { textResponse: '江戸川区', isCorrect: false },
+      { textResponse: '港区', isCorrect: false },
+      { textResponse: '荒川区', isCorrect: false },
+      { textResponse: '墨田区', isCorrect: true },
+    ]),
+  },
+  {
+    title: '日本三景である「天橋立」がある都道府県を答えよ',
+    options: arrayShuffle([
+      { textResponse: '岡山県', isCorrect: false },
+      { textResponse: '京都府', isCorrect: true },
+      { textResponse: '島根県', isCorrect: false },
+      { textResponse: '兵庫県', isCorrect: false },
+    ]),
+  },
+  {
+    title: 'リンゴの生産地が日本１位の都道府県を答えよ',
+    options: arrayShuffle([
+      { textResponse: '青森県', isCorrect: true },
+      { textResponse: '秋田県', isCorrect: false },
+      { textResponse: '岩手県', isCorrect: false },
+      { textResponse: '山形県', isCorrect: false },
+    ]),
+  },
+  {
+    title:
+      'プロ野球、中日ドラゴンズの本拠地である「バンテリンドーム」がある都道府県を答えよ',
+    options: arrayShuffle([
+      { textResponse: '福岡県', isCorrect: false },
+      { textResponse: '北海道', isCorrect: false },
+      { textResponse: '宮城県', isCorrect: false },
+      { textResponse: '愛知県', isCorrect: true },
+    ]),
+  },
+  {
+    title:
+      '1990年〜2017年まで存在していた宇宙のテーマパーク、「スペースワールド」があった都道府県を答えよ',
+    options: arrayShuffle([
+      { textResponse: '福岡県', isCorrect: true },
+      { textResponse: '岡山県', isCorrect: false },
+      { textResponse: '山梨県', isCorrect: false },
+      { textResponse: '宮城県', isCorrect: false },
+    ]),
+  },
+];
