5f4d22d49fc823802ee81f5fe0e3f721a8350067
furu0202 <furuka.0202@gmail.com>
2023-10-31 23:39:47 +0900

    fix:eslint不具合修正

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -3,25 +3,23 @@ import styles from './page.module.css';
 import Link from 'next/link';
 import { ITEMS } from '../components/Items';
 
-const Page = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>コース選択</h1>
-        <p>設定</p>
-        <div className={styles.boxArea}>
-          {ITEMS.map((item) => (
-            <div key={item.id} className={styles.itemGroup}>
-              <h2 className={styles.title}>{item.title}</h2>
-              <Link href={item.href} className={styles.button}>
-                {item.name}
-              </Link>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
-  );
-};
+const Page = () => (
+  <div className={styles.container}>
+    <main className={styles.main}>
+      <h1>コース選択</h1>
+      <p>設定</p>
+      <div className={styles.boxArea}>
+        {ITEMS.map((item) => (
+          <div key={item.id} className={styles.itemGroup}>
+            <h2 className={styles.title}>{item.title}</h2>
+            <Link href={item.href} className={styles.button}>
+              {item.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </main>
+  </div>
+);
 
 export default Page;
