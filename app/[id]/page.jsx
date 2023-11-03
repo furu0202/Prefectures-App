0242c78aa392aa426019f0b4b702f264adf50705
furu0202 <furuka.0202@gmail.com>
2023-11-04 01:57:37 +0900

    refactor:記述の追加

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.css';
 import { usePathname } from 'next/navigation';
 import { ITEMS } from '../components/Items';
@@ -8,30 +8,31 @@ import { ITEMS } from '../components/Items';
 const Page = () => {
   const pathname = usePathname();
   const pathData = ITEMS.find((item) => item.href === pathname);
-  const buttonAlert = () => {
-    alert('Click');
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const handleClick = () => {
+    if (currentQuestion < pathData.quizData.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+    }
   };
-  const choicesItem = ['選択肢A', '選択肢B', '選択肢C', '選択肢D'];
+
   return (
     <div className={styles.container}>
       <div className={styles.main}>
         <h1 className={styles.title}>{pathData.title}</h1>
-        <h1 className={styles.questionCount}>第X問 / 第Y問</h1>
+        <h1 className={styles.questionCount}>
+          第{currentQuestion}問 / 第{pathData.quizData.length}問
+        </h1>
         <h2 className={styles.totalCorrectAnswer}>現在の正解数:X問</h2>
-        <h2 className={styles.questionArea}>問題文{pathData.question}</h2>
+        <h2 className={styles.questionArea}>
+          {pathData.quizData[currentQuestion].title}
+        </h2>
         <div className={styles.choicesContainer}>
-          <p className={styles.choicesItem}>
-            <button onClick={buttonAlert}>選択肢A</button>
-          </p>
-          <p className={styles.choicesItem}>
-            <button onClick={buttonAlert}>選択肢B</button>
-          </p>
-          <p className={styles.choicesItem}>
-            <button onClick={buttonAlert}>選択肢C</button>
-          </p>
-          <p className={styles.choicesItem}>
-            <button onClick={buttonAlert}>選択肢D</button>
-          </p>
+          {pathData.quizData[currentQuestion].options.map((item, index) => {
+            <p key={index} className={styles.choicesItem}>
+              <button onClick={() => handleClick()}>{item.textResponse}</button>
+            </p>;
+          })}
         </div>
       </div>
     </div>
