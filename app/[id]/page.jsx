7523348878ac92b53ed86d387cc8cf44b26e64b3
furu0202 <furuka.0202@gmail.com>
2023-11-04 02:57:26 +0900

    feat:正解した際「現在の正解数」のカウントを増加させるよう対応

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -9,8 +9,12 @@ const Page = () => {
   const pathname = usePathname();
   const pathData = ITEMS.find((item) => item.href === pathname);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const handleClick = () => {
-    if (currentQuestion < pathData.quizData.length) {
+  const [count, setCount] = useState(0);
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setCount(count + 1);
+    }
+    if (currentQuestion < pathData.quizData.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
     } else {
     }
@@ -21,16 +25,18 @@ const Page = () => {
       <div className={styles.main}>
         <h1 className={styles.title}>{pathData.title}</h1>
         <h1 className={styles.questionCount}>
-          第{currentQuestion}問 / 第{pathData.quizData.length}問
+          第{currentQuestion + 1}問 / 第{pathData.quizData.length}問
         </h1>
-        <h2 className={styles.totalCorrectAnswer}>現在の正解数:X問</h2>
+        <h2 className={styles.totalCorrectAnswer}>現在の正解数:{count}問</h2>
         <h2 className={styles.questionArea}>
           {pathData.quizData[currentQuestion].title}
         </h2>
         <div className={styles.choicesContainer}>
           {pathData.quizData[currentQuestion].options.map((item, index) => (
             <p key={index} className={styles.choicesItem}>
-              <button onClick={() => handleClick()}>{item.textResponse}</button>
+              <button onClick={() => handleClick(item.isCorrect)}>
+                {item.textResponse}
+              </button>
             </p>
           ))}
         </div>
