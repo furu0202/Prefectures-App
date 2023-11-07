595e3c470cc5f984470d190d56f1d233b77c4537
furu0202 <furuka.0202@gmail.com>
2023-11-07 22:05:02 +0900

    feat: 新たにクイズの結果画面の雛形を作成

---
+++ app/reslut/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './page.module.css';
+
+const Page = () => (
+  <div className={styles.container}>
+    <div className={styles.main}>
+      <h1>クイズ結果発表</h1>
+    </div>
+  </div>
+);
+export default Page;
