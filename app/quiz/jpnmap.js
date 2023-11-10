c6897024c69a7d94314d27c6c4d1b23d42f60e75
furu0202 <furuka.0202@gmail.com>
2023-11-10 23:48:58 +0900

    feat:日本地図クイズの問題を追加

--- app/quiz/jpnmap.js
+++ app/quiz/jpnmap.js
@@ -1,6 +1,6 @@
 export const jpnMapQuizData = [
   {
-    title: '「富士山」という山がある都道府県はどこでしょうか',
+    title: '「富士山」という山がある都道府県は何県でしょうか',
     options: [
       { textResponse: '群馬', isCorrect: false },
       { textResponse: '山梨', isCorrect: true },
@@ -9,7 +9,7 @@ export const jpnMapQuizData = [
     ],
   },
   {
-    title: '日本で最も人口が多い都道府県は何ですか？',
+    title: '日本で最も人口が多い都道府県は何県ですか？',
     options: [
       { textResponse: '北海道', isCorrect: false },
       { textResponse: '東京', isCorrect: true },
@@ -18,7 +18,7 @@ export const jpnMapQuizData = [
     ],
   },
   {
-    title: '日本で最も面積が広い都道府県は何ですか',
+    title: '日本で最も面積が広い都道府県は何県ですか',
     options: [
       { textResponse: '沖縄', isCorrect: false },
       { textResponse: '東京', isCorrect: false },
@@ -27,7 +27,7 @@ export const jpnMapQuizData = [
     ],
   },
   {
-    title: '奈良時代に平城京が置かれた都道府県はどこですか？',
+    title: '奈良時代に平城京が置かれた都道府県は何県ですか？',
     options: [
       { textResponse: '京都', isCorrect: false },
       { textResponse: '大阪', isCorrect: false },
@@ -36,12 +36,48 @@ export const jpnMapQuizData = [
     ],
   },
   {
-    title: '日本で最も北に位置する都道府県は何ですか？',
+    title: '日本で最も北に位置する都道府県は何県ですか？',
     options: [
       { textResponse: '青森', isCorrect: false },
       { textResponse: '北海道', isCorrect: true },
       { textResponse: '沖縄', isCorrect: false },
       { textResponse: '東京', isCorrect: false },
+    ],
+  },
+  {
+    title: '本州で最も西端にある都道府県は何県ですか？',
+    options: [
+      { textResponse: '青森', isCorrect: false },
+      { textResponse: '山口', isCorrect: true },
+      { textResponse: '沖縄', isCorrect: false },
+      { textResponse: '愛知', isCorrect: false },
+    ],
+  },
+  {
+    title: '本初子午線がある都道府県は何県ですか？',
+    options: [
+      { textResponse: '宮城', isCorrect: false },
+      { textResponse: '福岡', isCorrect: false },
+      { textResponse: '兵庫', isCorrect: true },
+      { textResponse: '富山', isCorrect: false },
+    ],
+  },
+  {
+    title: '本初子午線がある都道府県は何県ですか？',
+    options: [
+      { textResponse: '宮城', isCorrect: false },
+      { textResponse: '福岡', isCorrect: false },
+      { textResponse: '兵庫', isCorrect: true },
+      { textResponse: '富山', isCorrect: false },
+    ],
+  },
+  {
+    title: '出雲大社がある都道府県は何県ですか？',
+    options: [
+      { textResponse: '宮崎', isCorrect: false },
+      { textResponse: '鹿児島', isCorrect: false },
+      { textResponse: '島根', isCorrect: true },
+      { textResponse: '熊本', isCorrect: false },
     ],
   },
 ];
